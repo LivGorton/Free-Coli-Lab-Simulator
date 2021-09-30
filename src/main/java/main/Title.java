ae508cc93b0e9f36f5513367aa7b8d918e1c9da5
Emily Cooper <e.cooper@sydney.edu.au>
2021-09-30 12:37:07 +1000

    changes

--- src/main/java/main/Title.java
+++ src/main/java/main/Title.java
@@ -22,14 +22,16 @@ public class Title {
         app.background(245, 242, 255);
         // this.startButton.draw(app);
         app.textFont(this.font,70);
-        app.image(this.logo, 40,300);                  
+        app.image(this.logo, 40,100, this.logo.width*2, this.logo.height*2);                  
         app.fill(0);  
         app.textAlign(app.CENTER);
         app.text("FreeColi",app.width/2 + 300,app.height/2 -200); 
+        app.textFont(this.font,25);
+        app.text("A natural transformation lab simulator", app.width/2 + 300, app.height/2 - 150);
         app.textAlign(app.LEFT);
         this.startButton.draw(app);
-
-    }
+        
+}
 
 
 
